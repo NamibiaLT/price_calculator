ec8ff01fbf0665b4beffa70d1bf7abdb3de2c2ed
NLT <NamibiaLT@users.noreply.github.com>
2018-07-16 11:47:09 -0700

    add to application class

---
+++ lib/application.rb
@@ -0,0 +1,21 @@
+require 'json'
+
+class Application
+  def initialize(cart_file, base_price_file, reporter=STDOUT)
+    @cart_file = cart_file
+    @base_price_file = base_price_file
+  end
+
+  def cli
+  end
+
+  private
+
+  def parsed_cart_file
+    JSON.parse(File.read(@cart_file))
+  end
+
+  def parsed_base_price_file
+    Json.parse(File.read(@base_price_file))
+  end
+end
