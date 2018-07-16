636e3fc8101c8168296244833380f879c2a3c7a3
NLT <NamibiaLT@users.noreply.github.com>
2018-07-16 11:45:32 -0700

    add more to cli method test

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -3,7 +3,14 @@ require 'application'
 
 RSpec.describe 'Application' do
   describe 'cli' do
-    let(:cart_file) { 'http://take-home-test.herokuapp.com/cart-4560.json' }
-    let(:base_price_file) { 'http://take-home-test.herokuapp.com/base-prices.json' }
+    let(:cart_file) { 'example-data/cart_4560.json' }
+    let(:base_price_file) { 'example-data/base_prices.json' }
+    let(:application) { Application.new(cart_file, base_price_file) }
+
+    it 'should print out the base price for one item' do
+      allow(STDOUT).to receive(:puts)
+      expect(STDOUT).to receive(:puts).with('Product: hoodie, Colour: white, dark, Size: small, Price: $38.00')
+      application.cli
+    end
   end
 end
