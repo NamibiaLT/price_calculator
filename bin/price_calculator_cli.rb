103947a5b4e0033a2d7abe90c5235a42a41ec0de
NLT <NamibiaLT@users.noreply.github.com>
2018-07-18 20:21:49 -0700

    update cli

--- bin/price_calculator_cli.rb
+++ bin/price_calculator_cli.rb
@@ -6,12 +6,10 @@ $:.unshift File.join(__FILE__, '..', '..', 'lib')
 base_prices_file = ARGV[0]
 cart_file = ARGV[1]
 
-  puts 'Hello World'
-
-# if base_prices_file.nil? || cart_file.nil?
-#   # puts "Usage: #{__FILE__} /path/to/base_prices/file /path/to/cart/file"
-#   exit 2
-# end
+if base_prices_file.nil? || cart_file.nil?
+  puts "Remember to put: #{__FILE__} /path/to/base_prices/file /path/to/cart/file"
+  exit 2
+end
 
 require 'application'
 
