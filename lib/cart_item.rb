220f0dc29f692826388d8541c95b4e10c9d948cf
NLT <NamibiaLT@users.noreply.github.com>
2018-07-20 13:02:46 -0700

    add method

--- lib/cart_item.rb
+++ lib/cart_item.rb
@@ -2,9 +2,12 @@ require 'json'
 
 class CartItem
   attr_reader :product_type, :details
-  
+
   def initialize(product_type:, details:)
     @product_type = product_type
     @details = details
+  end
+
+  def match?
   end
 end
