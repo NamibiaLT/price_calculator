9e0a03871521ec5b9378a8c63aac29465d043a77
NLT <NamibiaLT@users.noreply.github.com>
2018-07-20 12:05:40 -0700

    change spec

--- spec/cart_parser_spec.rb
+++ spec/cart_parser_spec.rb
@@ -5,29 +5,14 @@ require 'cart_parser'
 RSpec.describe 'CartParser' do
   let(:cart_parser) { CartParser.new('./example-data/cart_4560.json') }
   let(:item) { cart_parser.parse }
-  let(:option) { 
-    {
-      "size": "small",
-      "colour": "white",
-      "print-location": "front"
-    }
-  }
 
   describe 'parse' do
     it 'sets the product type for an item' do
       expect(item[0].product_type).to eq('hoodie')
     end
 
-    it 'sets the artist markup for an item' do
-      expect(item[0].markup).to eq(20)
-    end    
-    
-    it 'sets options for an item' do
-      expect(item[0].options).to eq(option)
-    end
-
-    it 'sets the quantity for an item' do
-      expect(item[0].quantity).to eq(1)
+    it 'sets the product details for an item' do
+      expect(item[0].details[0].markup).to eq(20)
     end
   end
 end
