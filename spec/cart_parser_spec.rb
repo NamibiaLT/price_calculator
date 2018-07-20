b1f69e6a19d0a4e8e93c4bcd7b334db2b0a6650a
NLT <NamibiaLT@users.noreply.github.com>
2018-07-20 09:32:33 -0700

    update specs

---
+++ spec/cart_parser_spec.rb
@@ -0,0 +1,32 @@
+require 'rspec'
+require 'cart_parser'
+
+RSpec.describe 'CartParser' do
+  let(:cart_parser) { CartParser.new(@filename) }
+  let(:item) { cart_parser.parse }
+  let(:option) { 
+    {
+      "size": "small",
+      "colour": "white",
+      "print-location": "front"
+    }
+  }
+
+  describe 'parse' do
+    it 'sets the product type for an item' do
+      expect(item[0].product_type).to eq('hoodie')
+    end
+
+    it 'sets the artist markup for an item' do
+      expect(item[0].markup).to eq(20)
+    end    
+    
+    it 'sets options for an item' do
+      expect(item[0].options).to eq(option)
+    end
+
+    it 'sets the quantity for an item' do
+      expect(item[0].quantity).to eq(1)
+    end
+  end
+end
