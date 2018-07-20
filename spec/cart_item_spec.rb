f6016a6d0d8ec43be86be96ebb681930b8416acd
NLT <NamibiaLT@users.noreply.github.com>
2018-07-20 13:01:52 -0700

    update spec

--- spec/cart_item_spec.rb
+++ spec/cart_item_spec.rb
@@ -1,2 +1,20 @@
 require 'json'
 require 'cart_item'
+
+RSpec.describe 'CartItem' do
+  let(:option) { {"size": "small", "colour": "white", "print-location": "front"} }
+  let(:item_details) { Cart.new(options: option, markup: 20, quantity: 1) }
+  let(:item) { CartItem.new(product_type: 'hoodie', details: item_details)}
+
+  describe 'match?' do
+    it 'matches the product type to the details' do
+      expect(item.match?('hoodie')).to be true
+    end
+  end
+
+  describe 'item_details_for' do
+    it 'finds the item details when given options for an item' do
+      expect(item.item_details_for(option)).to be(item_details)
+    end
+  end
+end
