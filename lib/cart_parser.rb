cff97f02d1e098d22a13b15c6b41b8c4febf7cb0
NLT <NamibiaLT@users.noreply.github.com>
2018-07-20 11:13:38 -0700

    add new class

--- lib/cart_parser.rb
+++ lib/cart_parser.rb
@@ -1,1 +1,23 @@
 require 'json'
+
+class CartParser
+  def initialize(cart_file)
+    @cart_file = cart_file
+  end
+
+  def parse
+    JSON.parse(File.read(@cart_file))
+      .group_by {|hash| hash['product-type'] }
+      .map do |product_type, product_hashes|
+        Item.new(product_type: product_type, details: details(product_hashes))
+      end
+  end
+
+  private
+
+  def details(product_hashes)
+    product_hashes.map do |product_hash|
+      Cart.new(options: product_hash['options'], markup: product_hash['artist-markup'], quantity: product_hash['quantity'])
+    end
+  end
+end
