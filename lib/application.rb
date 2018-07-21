f6bcd40424536a3a632d3897a80ff93d3051f89f
NLT <NamibiaLT@users.noreply.github.com>
2018-07-21 13:02:24 -0700

    remove unneccessary code

--- lib/application.rb
+++ lib/application.rb
@@ -8,7 +8,7 @@ class Application
 
   def cli
     cart_item.total.each do |item|
-      STDOUT.puts "#{cart_item.product_type} total: #{cart.total} cents \n"
+      STDOUT.puts "#The total for all the items in your cart is: #{cart.total} cents \n"
     end
   end
 end
--- lib/cart_item.rb
+++ lib/cart_item.rb
@@ -1,8 +1,4 @@
-require 'json'
-
 class CartItem
-  attr_reader :product_type, :details
-
   def initialize(product_type:, details:)
     @product_type = product_type
     @details = details
