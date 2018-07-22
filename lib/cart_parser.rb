f91e8ec9f91083740ebd8bca4727a2bc62e390fd
NLT <NamibiaLT@users.noreply.github.com>
2018-07-22 11:12:37 -0700

    only one cart for all items

--- lib/cart_parser.rb
+++ lib/cart_parser.rb
@@ -7,17 +7,17 @@ class CartParser
 
   def parse
     JSON.parse(File.read(@cart_file))
+      binding.pry
       .group_by {|hash| hash['product-type'] }
       .map do |product_type, product_hashes|
-        CartItem.new(product_type: product_type, details: details(product_hashes))
+        CartItems.new(product_type: product_type, details: details(product_hashes))
       end
   end
 
   private
 
   def details(product_hashes)
-    product_hashes.map do |product_hash|
-      Cart.new(options: product_hash['options'], markup: product_hash['artist-markup'], quantity: product_hash['quantity'])
-    end
+    product_hashes.map 
+    { |product_hash| options: product_hash['options'], markup: product_hash['artist-markup'], quantity: product_hash['quantity'] }
   end
 end
