3d8428b842845ed4858872450a594a8c843de819
NLT <NamibiaLT@users.noreply.github.com>
2018-07-23 15:30:59 -0700

    reformat

--- lib/cart_parser.rb
+++ lib/cart_parser.rb
@@ -6,18 +6,14 @@ class CartParser
   end
 
   def parse
-    binding.pry
     JSON.parse(File.read(@cart_file))
-      .group_by {|hash| hash['product-type'] }
-      .map do |product_type, product_hash|
-        CartItems.new(product_type: product_type, options: product_hash['options'], markup: product_hash['artist-markup'], quantity: product_hash['quantity'])
+      .map do |product_hash|
+        CartItems.new(
+          product_type: product_hash['product-type'],
+          options: product_hash['options'],
+          markup: product_hash['artist-markup'],
+          quantity: product_hash['quantity']
+        )
       end
   end
-
-  private
-
-  # def details(product_hashes)
-  #   binding.pry
-  #   product_hashes.map { |product_hash| options: product_hash['options'], markup: product_hash['artist-markup'], quantity: product_hash['quantity'] }
-  # end
 end
