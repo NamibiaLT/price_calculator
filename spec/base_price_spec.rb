dd887c6a3950f91bf503abd6c3a3dfc0cf22bf44
NLT <NamibiaLT@users.noreply.github.com>
2018-07-23 20:51:05 -0700

    remove unnecessary code

--- spec/base_price_spec.rb
+++ spec/base_price_spec.rb
@@ -1,11 +1,9 @@
 require 'rspec'
-require 'product'
 require 'base_price'
 
 RSpec.describe 'BasePrice' do
   let(:option) { { "size": ["large"], "colour": ["white"] } }
   let(:product_base_price) { BasePrice.new(price: 3848, options: option) }
-  let(:product) { Product.new(product_type: 'hoodie', base_prices: product_base_price) }
 
   describe 'match?' do
     it 'matches the price to the selected product based on the options' do
--- spec/cart_parser_spec.rb
+++ spec/cart_parser_spec.rb
@@ -10,9 +10,5 @@ RSpec.describe 'CartParser' do
     it 'sets the product type for an item' do
       expect(item[0].product_type).to eq('hoodie')
     end
-
-    it 'sets the product details for an item' do
-      expect(item[0].details[0].markup).to eq(20)
-    end
   end
 end
