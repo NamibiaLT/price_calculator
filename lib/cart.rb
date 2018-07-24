ed5e6c685b92eed742de8bff3d6d2b4f78080c34
NLT <NamibiaLT@users.noreply.github.com>
2018-07-24 14:07:55 -0700

    delete unnecessary code

--- lib/cart.rb
+++ lib/cart.rb
@@ -5,23 +5,19 @@ require 'base_price'
 class Cart
   attr_reader :options, :markup, :quantity
   
-  def initialize(options:, markup:, quantity:)
+  def initialize(product_type:, options:, markup:, quantity:)
+    @product_type = product_type
     @options = options
     @markup = markup
     @quantity = quantity
   end
 
-  def cart_item_to_product
-    Product.new(@product_type, @base_prices).base_price_for(options).include?(Cart.new(@options, @markup, @quantity).options)
+  def cart_total
+    
   end
 
-  def total
-    total = (BASEPRICE + (BASEPRICE * MARKUP)) * QUANTITY
-  end
+  private
 
-  private 
 
-  # BASEPRICE = BasePrice.new()
-  # MARKUP = (CartItem.new().item_markup_for(options))/100
-  # QUANTITY = (CartItem.new().item_quantity_for(options))
+
 end
