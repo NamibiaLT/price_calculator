c974d0def971a75ded821b03fa0aaeabc39eb81a
NLT <NamibiaLT@users.noreply.github.com>
2018-07-24 14:09:14 -0700

    update spec

--- spec/cart_spec.rb
+++ spec/cart_spec.rb
@@ -1,15 +1,14 @@
-# require 'rspec'
-# require 'cart_items'
-# require 'cart'
+require 'rspec'
+require 'cart_items'
+require 'cart'
 
-# RSpec.describe 'Cart' do
-#   let(:option) { {"size": "small", "colour": "white", "print-location": "front"} }
-#   let(:item) { CartItems.new(product_type: 'hoodie', options: option, markup: 20, quantity: 1)}
+RSpec.describe 'Cart' do
+  let(:option) { {"size": "small", "colour": "white", "print-location": "front"} }
+  let(:item) { Cart.new(product_type: 'hoodie', options: option, markup: 20, quantity: 1)}
 
-#   describe 'cart_total' do
-#     pending
-#     it 'finds the total price of the cart' do
-#       expect(item.cart_total).to eq(4560)
-#     end
-#   end
-# end
+  describe 'cart_total' do
+    it 'iterates through cart item prices and finds the total price of the cart' do
+      expect(item.cart_total).to eq(4560)
+    end
+  end
+end
