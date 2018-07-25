78a0de2ab82d67ceb6085c87abb1422013746356
NLT <NamibiaLT@users.noreply.github.com>
2018-07-25 09:12:50 -0700

    update spec to reflect two cart items and one cart

--- spec/cart_spec.rb
+++ spec/cart_spec.rb
@@ -1,13 +1,12 @@
 require 'rspec'
-require 'cart_items'
+require 'cart_item'
 require 'cart'
-require 'pry'
 
 RSpec.describe 'Cart' do
   let(:option1) { {"size": "small", "colour": "white", "print-location": "front"} }
-  let(:cart_item1) { CartItems.new(product_type: 'hoodie', options: option1, markup: 20, quantity: 1).item_total(20, 1) }
-  let(:cart_item2) { nil }
-  let(:cart) { Cart.new(cart_item1, cart_item2) }
+  let(:cart_item1) { CartItem.new(product_type: 'hoodie', options: option1, markup: 20, quantity: 1) }
+  let(:cart_item2) { CartItem.new(product_type: 'hoodie', options: option1, markup: 10, quantity: 2) }
+  let(:cart) { Cart.new([cart_item1]) }
   describe 'cart_total' do
     it 'iterates through cart item prices and finds the total price of the cart' do
       expect(cart.cart_total).to eq(4560)
