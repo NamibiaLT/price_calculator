5e9ae75e23fbd91006b2feceed280fc7f325f3c4
NLT <NamibiaLT@users.noreply.github.com>
2018-07-25 11:06:43 -0700

    minor syntax edit

--- bin/price_calculator_cli.rb
+++ bin/price_calculator_cli.rb
@@ -7,9 +7,7 @@ base_prices_file = ARGV[0]
 cart_file = ARGV[1]
 
 if base_prices_file.nil? || cart_file.nil?
-  puts
-  "Remember to put: #{__FILE__} /path/to/base_prices/file /path/to/cart/file"
-
+  puts "Remember to put: #{__FILE__} /path/to/base_prices/file /path/to/cart/file"
   exit 2
 end
 
