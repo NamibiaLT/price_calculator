b5b5cf5f449849e1937190f284dac8f8e1c98ba0
NLT <NamibiaLT@users.noreply.github.com>
2018-07-25 09:26:59 -0700

    delete unnecessary code

--- lib/cart.rb
+++ lib/cart.rb
@@ -8,14 +8,4 @@ class Cart
   def cart_total
     @cart_items.map(&:item_total).sum
   end
-
-  private
-
-  def cart_item_price
-    CartItem.new(product_type, options, markup, quantity).item_total(markup, quantity)
-  end
-
-  def parsed_cart_file(cart_file)
-    CartParser.new(@cart_file).parse
-  end
 end
