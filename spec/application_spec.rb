2d4d164f3ee61a66d8adc8c4cc93d905a9190c9b
NLT <NamibiaLT@users.noreply.github.com>
2018-07-25 17:40:51 -0700

    extend path

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -3,8 +3,8 @@ require 'application'
 
 RSpec.describe 'Application' do
   describe 'cli' do
-    let(:cart_file) { 'example-data/cart_4560.json' }
-    let(:base_price_file) { 'example-data/base_prices.json' }
+    let(:cart_file) { './example-data/cart_4560.json' }
+    let(:base_price_file) { './example-data/base_prices.json' }
     let(:application) { Application.new(cart_file, base_price_file) }
 
     it 'should print out the base price for one item' do
